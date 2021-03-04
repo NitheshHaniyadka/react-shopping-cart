d92a011ce11c97fb997f7ee9b41363e9f23aba42
NitheshHaniyadka <nithesh.haniyadka@yahoo.com>
2021-03-04 23:01:49 +0530

    product page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 //feature
 
+import { render } from '@testing-library/react';
 import React from 'react';
+import Products from './components/Products'
 
-function App() {
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      productslist:[],
+      filterCategory:"",
+      sortdiscount:""
+    }
+  }
+  
+render(){
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping</a>
       </header>
-      <main>Product List</main>
-      <footer>All right reserved</footer>
+      <div>
+      <Products />
 
+        
+      </div>
+      <footer>All right reserved</footer>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+
+class Products extends Component{
+    constructor(){
+        super();
+        this.state={
+            productList:[]        }
+    }
+
+    getRecipe=async ()=>{
+        const api_call=await fetch(`https://staging.healthandglow.com/api/catalog/product/v6/search/999?app=web&version=3.0.2&tag=loreal-paris&page=0:20`)
+    
+        const data = await api_call.json();
+        // console.log(productList.data.products[0].skuId);
+        this.setState({productList:data.data.products})
+        console.log(this.state.productList);
+      }
+    
+      componentDidMount(){
+        this.getRecipe()
+    
+      }
+
+    render(){
+        return<div className="container">
+                <div className="col-md-6 form-group">
+                    <div className="border p-3">
+                        {
+                            this.state.productList.map((xproduct,index)=>{
+                                return<div key={xproduct.skuId} className="border p-3">
+                                    <img src={xproduct.skuImageUrl} alt={xproduct.categoryName} height='120' width='150'/>
+                                    <p>{xproduct.parentCategoryName}</p>
+                                    </div>
+                            })
+                        }
+                        </div>
+                        </div>
+                       
+            </div>
+    }
+}
+
+export default Products
