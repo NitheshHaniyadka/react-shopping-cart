39c841c3823806802895a0027b359a3c8d2a11ae
NitheshHaniyadka <nithesh.haniyadka@yahoo.com>
2021-03-04 12:43:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping</a>
+      </header>
+      <main>Product List</main>
+      <footer>All right reserved</footer>
+
+    </div>
+  );
+}
+
+export default App;
